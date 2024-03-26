a8f57da284562a83e4adfbcf260797ee2e44c188
caixiiaoyang <53438281+caixiiaoyang@users.noreply.github.com>
2024-03-26 21:47:43 +0800

    A two-layer neural network is implemented

--- grad.py
+++ grad.py
@@ -6,7 +6,7 @@ def numerical_diff(f, x):
     return (f(x + h) - f(x - h)) / (2 * h)
 
 
-def numerical_gradient(f, x):
+def numerical_gradient1d(f, x):
     h = 1e-4
     grad = np.zeros_like(x)
     for idx in range(x.size):
@@ -24,11 +24,31 @@ def numerical_gradient(f, x):
     return grad
 
 
+def numerical_gradient2d(f, x):
+    grads = np.zeros_like(x)
+    if x.ndim == 1:
+        return numerical_gradient1d(f, x)
+    for idx in range(len(x)):
+        grad = numerical_gradient1d(f, x[idx])
+        grads[idx] = grad
+    return grads
+
+
+def numerical_gradient(f, x):
+    grads = np.zeros_like(x)
+    if x.ndim == 1 or x.ndim == 2:
+        return numerical_gradient2d(f, x)
+    for idx in range(len(x)):
+        grad = numerical_gradient2d(f, x[idx])
+        grads[idx] = grad
+    return grads
+
+
 def gradient_descent(f, init_x, lr=1e-2, step=100):
     x = init_x
 
     for i in range(step):
         grad = numerical_gradient(f, x)
-        x -= lr*grad
+        x -= lr * grad
 
     return x
